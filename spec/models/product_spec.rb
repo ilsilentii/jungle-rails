6e539ea4c69c28d16172cf659d54c3b464a87d45
Unknown <travis.vdm@outlook.com>
2017-10-16 17:21:34 -0600

    Merge branch 'feature/product-specs'
    changed validation corrections

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,42 +2,40 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    before do
-      @category = Category.create(name: "category")
-      @product = Product.new
-      @product.name = "something"
-      @product.price = 2000
-      @product.quantity = 2000
-      @product.category_id = @category.id
-      @product.valid?
+    before(:each) do
+      @category = Category.new(name: 'furniture')
     end
 
-    it 'should create a product' do
-      expect(@product).to be_valid
+    it "is valid if it has all attributes" do
+      @product = Product.new(name: 'chair', price: 50, quantity: 2, category: @category)
+      expect(@product.valid?).to be true
     end
 
-    it 'shouldnt have an empty name' do
-      @product.name = nil
-      @product.save
-      expect(@product.errors.full_messages[0]).to include(@product.errors[:name][0])
+    it 'should not be valid if it doesnt have a name' do
+      @product = Product.new(price: 50, quantity: 2, category: @category)
+      expect(@product.valid?).to be false
+      expect(@product.errors.full_messages).to include("Name can't be blank")
+
     end
 
-    it 'shouldnt have an empty price' do
-      @product.price_cents = nil
-      @product.save
-      expect(@product.errors.full_messages[1]).to include(@product.errors[:price][0])
+    it 'should not be valid if it doesnt have quantity specified' do
+      @product = Product.new(name: "chair", price: 50, category: @category)
+      expect(@product.valid?).to be false
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+
     end
 
-    it 'shouldnt have an empty quantity' do
-      @product.quantity = nil
-      @product.save
-      expect(@product.errors.full_messages[0]).to include(@product.errors[:quantity][0])
+    it 'should not be valid if it doesnt have a price' do
+      @product = Product.new(name: "chair", quantity: 2, category: @category)
+      expect(@product.valid?).to be false
+      expect(@product.errors.full_messages).to include("Price can't be blank")
+
     end
 
-    it 'shouldnt have an empty category' do
-      @product.category_id = nil
-      @product.save
-      expect(@product.errors.full_messages[0]).to include(@product.errors[:category][0])
+    it 'should not be valid if it doesnt have a category' do
+      @product = Product.new(name: "chair", price: 50, quantity: 2)
+      expect(@product.valid?).to be false
+      expect(@product.errors.full_messages).to include("Category can't be blank")
     end
 
   end
